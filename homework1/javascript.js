188c22a67bcb67573a8e538bcb748c2fbc5455ab
AnnaTh <tcybulnyk2201@gmail.com>
2023-01-14 22:20:33 +0200

    i did hw1

---
+++ homework1/javascript.js
@@ -0,0 +1,40 @@
+let messege;
+
+messege = 'hello!';
+
+console.log(messege);
+alert(messege);
+
+let userAge;
+let userName;
+let userSurname;
+
+userAge = '10';
+userName = 'Ann';
+userSurname = 'Tarasova';
+
+console.log(userAge);
+console.log(userName);
+console.log(userSurname);
+
+let newUser;
+let listPurchase;
+let userOnline;
+let employeeSalary;
+let cursorCoordinates;
+
+let greetings;
+
+greetings = 'Hello World!';
+
+console.log(greetings);
+
+
+console.log(typeof 123);
+
+const userAgeNew = 12;
+console.log('userAge', userAgeNew);
+
+
+const petName = 'Jack';
+console.log('My dog name', petName); 
