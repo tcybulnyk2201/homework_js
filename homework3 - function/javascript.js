b11461ca61c0dee4a9579e10ff84341e8679bf01
AnnaTh <tcybulnyk2201@gmail.com>
2023-01-27 22:31:58 +0200

    add hm

--- homework3 - function/javascript.js
+++ homework3 - function/javascript.js
@@ -86,5 +86,33 @@ function greetings() {
 greetings();
 greetings();
 
+// Задание 4
 
+const whichDay1 = Number(prompt('Введите число, чтобы узнать день недели'));
+
+switch (whichDay1) {
+    case 1: 
+        alert('Понедельник');
+        break;
+    case 2:
+        alert('Вторник');
+        break;
+    case 3:
+        alert('Среда');
+        break;
+    case 4:
+        alert('Четверг');
+        break;
+    case 5:
+        alert('Пятница');
+        break;
+    case 6:
+        alert('Суббота');
+        break;
+    case 7:
+        alert('Воскресенье');
+        break;
+    default:
+        alert('Такого дня нет') 
+}
 
